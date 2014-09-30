5548b3044ddc155f0a7d2d4169860e302040e0b4
Michael Fisher <michael@thefisherhome.net>
2014-09-29 21:13:18 -0500

    Exercise 2 completed.

---
+++ ex2.py
@@ -0,0 +1,9 @@
+#This is an example of using multi-line comments in Python code.
+#This could be helpful in describing what's happening in the code.
+
+print "I could have code like this."
+
+#Comments can also be used to temporarily disable secitons of code.
+
+#print "This won't run."
+print "This will run."
